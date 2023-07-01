74f8400d4bb6544db41cbbd19af4f30757f0bf3e
A007-cloud <dasaditya788@gmail.com>
2023-07-01 16:33:16 +0530

    first commit

---
+++ Linked List/doublyLinkedList.js
@@ -0,0 +1,50 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.tail = null;
+    this.prev = null;
+  }
+}
+
+class doublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    var newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined;
+    var popedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = popedNode.prev;
+      this.tail.next = null;
+    }
+    this.length--;
+    return popedNode;
+  }
+}
+
+var list = new doublyLinkedList();
+list.push(36);
+list.push(55);
+console.log(list);
+
+list.pop();
+
+console.log(list);
